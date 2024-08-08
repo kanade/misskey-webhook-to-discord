5980ad367efd247abebe86801e96c9f30c48bda9
kanade <pokoteng@gmail.com>
2024-08-08 21:09:15 +0900

    新規登録時にユーザー名がnullになるのを防いだつもり

--- src/index.ts
+++ src/index.ts
@@ -123,7 +123,7 @@ app.post('/api/webhooks/:id/:token', async r => {
 		case 'userCreated': {
 			embed.setColor(0xcb9a11)
 			embed.setTitle('User created')
-			embed.setDescription(`User created: [${payload.body.name}](${payload.server}/@${payload.body.username})`)
+			embed.setDescription(`User created: [${payload.body.name} ?? ${payload.body.username}](${payload.server}/@${payload.body.username})`)
 			break
 		}
 	}
